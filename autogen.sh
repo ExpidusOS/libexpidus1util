04650ada5b5db983242a3bff6516a2ddeda210e2
TheComputerGuy <rosscomputerguy@protonmail.com>
2021-01-15 19:17:12 -0800

    Fixed error message

--- autogen.sh
+++ autogen.sh
@@ -25,10 +25,10 @@
 
 (type xdt-autogen) >/dev/null 2>&1 || {
   cat >&2 <<EOF
-autogen.sh: You don't seem to have the Expidus development tools installed on
+autogen.sh: You don't seem to have the XFCE development tools installed on
             your system, which are required to build this software.
-            Please install the expidus1-dev-tools package first, it is available
-            from http://www.expidus.org/.
+            Please install xfce4-dev-tools package first, it is available
+            from http://www.xfce.org/.
 EOF
   exit 1
 }
